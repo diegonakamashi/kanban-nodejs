806266af7c73148dd8908901e6c83dbefd4e837f
Diego Nakamashi <dnaka@bananal.(none)>
2012-06-23 12:56:03 -0300

    Retirada de método que não esta sendo usado

--- model/kanban.js
+++ model/kanban.js
@@ -3,11 +3,6 @@ var Step = require('step');
 var Util = require('util');
 
 
-module.exports.save = function(kanban){
-	conn.execute('INSERT INTO kanban SET TITLE= ' + kanban.title + ', DESCRIPTION = ' + kanban.description);
-}
-
-
 module.exports.findByUserId = function(userId, callBack){
 	var query = 'SELECT k.* FROM kanban k, user_kanban uk WHERE k.id = uk.kanban_id AND uk.user_id = ' + userId;
 	Step(
